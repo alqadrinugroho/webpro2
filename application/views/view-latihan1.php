91fa5f1888aec6ba4fe224e31ff74a8adfc70ddf
alqadrinugroho <alqadrinugroho@gmail.com>
2023-04-02 20:21:04 +0700

    updated

--- application/views/view-latihan1.php
+++ application/views/view-latihan1.php
@@ -1,11 +1,11 @@
 <html>
 <head>
-<title>Latihan 1</title>34
+<title>Latihan 1</title>
 </head>
 <body>
 Halo Kawan.. Yuk kita belajar web programming..!!!<br>
-Nilai 1 = <?= $nilai1; ?>
-Nilai 2 = <?= $nilai2; ?>
-ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
+Nilai 1 = <?= $nilai1; ?><br>
+Nilai 2 = <?= $nilai2; ?><br>
+Ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
 $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
 </body>
